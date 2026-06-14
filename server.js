d93058b132593d24ff022c792e6327ab0661943a
cepicahyana <cahyanacepi@gmail.com>
2026-06-15 01:30:25 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    console.log('Browser connected');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server running');
+});
